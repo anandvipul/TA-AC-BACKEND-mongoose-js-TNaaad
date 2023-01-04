a5c4fa31aebd949f64b07a0c66f7117a7c8f4830
Vipul Anand <anandvipul01@gmail.com>
2023-01-04 17:17:09 +0530

    Schema in Momgose Part 2

--- block-BNaacd/app/models/user.js
+++ block-BNaacd/app/models/user.js
@@ -5,7 +5,7 @@ let Schema = mongoose.Schema;
 let user = new Schema(
     {
         name: {type: String },
-        email: {type: String},
-        age: {type: Number}
+        email: {type: String, lowerCase: true},
+        age: {type: Number, default: 0}
     }
 );
