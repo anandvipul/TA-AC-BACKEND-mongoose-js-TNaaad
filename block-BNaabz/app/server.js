68ba3bbbefa72904484bfdf656c2e49b84f82cd5
anandvipul <anandvipul01@gmail.com>
2023-01-04 11:47:47 +0530

    fix

--- block-BNaabz/app/server.js
+++ block-BNaabz/app/server.js
@@ -1,7 +1,11 @@
 let express = require("express");
 let mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/test", (err) => {
+mongoose.connect("mongodb://localhost:27017/test", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  }, (err) => {
     console.log(err ? err : "Connected");
 });
 
