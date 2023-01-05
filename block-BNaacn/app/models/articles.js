326964df4a654819528b5b4cb2e72b2bce65df57
Vipul Anand <anandvipul01@gmail.com>
2023-01-05 11:53:46 +0530

    article schema

---
+++ block-BNaacn/app/models/articles.js
@@ -0,0 +1,13 @@
+let mongoose = require("mongoose");
+
+let Schema = mongoose.Schema;
+
+let article = new Schema (
+    {
+        title: {type: String, required: true, default: "A Beautiful Post"},
+        description: {type: String, default: "A beautiful Description"},
+        tags: [String],
+        createdAt: {type: Date, default: new Date()},
+        likes: {type: Number, default: 0}
+    }
+);
